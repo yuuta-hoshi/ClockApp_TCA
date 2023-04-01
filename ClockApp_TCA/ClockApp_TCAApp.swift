1cff7bcdcd2290e0b426b89913c77e8834e49571
yuuta-hoshi <h_y0608@icloud.com>
2023-04-01 23:40:44 +0900

    Initial Commit

---
+++ ClockApp_TCA/ClockApp_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp_TCAApp.swift
+//  ClockApp_TCA
+//
+//  Created by 星優大 on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockApp_TCA/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClockApp_TCA
+//
+//  Created by 星優大 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
